407a29edfd7c8c2ed04015af2c14c3a6e9d91aae
FieryRider <30357809+FieryRider@users.noreply.github.com>
2021-03-30 22:11:08 +0300

    Clear Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,10 @@
 <template>
-  <h1>Home page</h1>
-  <!--
-  <v-container>
-    <v-row>
-      <v-col
-        v-for="n in 24"
-        :key="n"
-        cols="4"
-      >
-        <v-card height="200"></v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-  -->
+  <div></div>
 </template>
 
 <script>
 export default {
-
-}
+};
 </script>
 
 <style>

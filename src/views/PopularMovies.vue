abdd0dbfe517d0cc945d1323da8175c1572067ea
FieryRider <30357809+FieryRider@users.noreply.github.com>
2021-04-01 07:58:25 +0300

    Make 'Add to favourite'/'Remove favourite' buttons work currectly in Popular Movies page

--- src/views/PopularMovies.vue
+++ src/views/PopularMovies.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="movie-grid">
-    <MovieCard v-for="movie in movies" :key="movie.movieId" color="white" :title="movie.title" :releaseDate="movie.releaseDate" :rating="movie.rating" :posterUrl="movie.posterUrl" :movieId="movie.movieId" />
+    <MovieCard v-for="movie in movies" :key="movie.movieId" color="white" :title="movie.title" :releaseDate="movie.releaseDate" :rating="movie.rating" :posterUrl="movie.posterUrl" :favourite="movie.favourite" :movieId="movie.movieId" />
   </main>
 </template>
 
@@ -16,8 +16,39 @@ export default {
       movies: []
     }
   },
+  computed: {
+    isUserLogged: function() {
+      if (this.$store.getters.getUserToken == null)
+        return false
+
+      return true
+    },
+  },
   created: function() {
-    fetch("https://eu-api.backendless.com/8764A135-6D4C-0237-FF3B-E041AA778300/A5DE6895-9860-4194-A9BD-99EC35D4131D/data/movies")
+    let favouriteMoviesIds = undefined
+    let favouriteMoviesRequest = Promise.resolve()
+    if (this.isUserLogged) {
+      favouriteMoviesRequest = fetch("https://eu-api.backendless.com/8764A135-6D4C-0237-FF3B-E041AA778300/A5DE6895-9860-4194-A9BD-99EC35D4131D/data/favourite_movies?relationsDepth=1", {
+        headers: {
+          'Conten-Type': "application/json",
+          'user-token': this.$store.getters.getUserToken
+        }
+      })
+    }
+    favouriteMoviesRequest
+      .then(resp => {
+        if (resp === undefined)
+          return Promise.resolve()
+        else
+          return resp.json()
+      })
+      .then(data => {
+        favouriteMoviesIds = null
+        if (data != undefined)
+          favouriteMoviesIds = data.map(favMovie => favMovie.movie.objectId)
+
+        return fetch("https://eu-api.backendless.com/8764A135-6D4C-0237-FF3B-E041AA778300/A5DE6895-9860-4194-A9BD-99EC35D4131D/data/movies")
+      })
       .then(resp => resp.json())
       .then(data => {
         data.forEach(dbMovie => {
@@ -31,7 +62,8 @@ export default {
             'budget': dbMovie.budget,
             'revenue': dbMovie.revenue,
             'status': dbMovie.status,
-            'movieId': dbMovie.objectId
+            'movieId': dbMovie.objectId,
+            'favourite': favouriteMoviesIds == null ? false : favouriteMoviesIds.includes(dbMovie.objectId)
           }
 
           this.movies.push(movie)

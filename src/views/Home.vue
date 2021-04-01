1922d3a2830c33e3949dbf4c907a244e621cb2ac
FieryRider <30357809+FieryRider@users.noreply.github.com>
2021-04-01 12:28:58 +0300

    Add Home page and fix font-family

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <header class="o-content-wrapper">
+    <h1>Welcome</h1>
+    <h2>Millions of movies, TV shows and people to discover. Explore now.</h2>
+  </header>
 </template>
 
 <script>
@@ -7,5 +10,23 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+h1, h2, h3 {
+  color: #fff;
+  margin-left: 30px;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+h1 {
+  font-size: 3rem;
+}
+h2 {
+  font-size: 2rem;
+}
+header {
+  display: grid;
+  align-content: center;
+  background-image: url('https://www.themoviedb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,032541,01b4e4)/34OGjFEbHj0E3lE2w0iTUVq0CBz.jpg');
+  height: 400px;
+}
 </style>
